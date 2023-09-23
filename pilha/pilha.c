f15a284b901ac6bcc9eb4e8e080b0a641a108a0c
João <jpmacleure@gmail.com>
2023-09-23 07:45:05 -0300

    fix pilha

--- pilha/pilha.c
+++ pilha/pilha.c
@@ -3,10 +3,9 @@
 #include "pilha.h"
 
 Pilha* pilha(){
-    Pilha p;
-    p.topo = NULL;
-    p.quantidade = 0;
-    Pilha* p1 = &p;
+    Pilha *p1 = (Pilha*) malloc(sizeof(Pilha));
+    p1->topo = NULL;
+    p1->quantidade = 0;
     return p1;
 }
 
@@ -14,12 +13,15 @@ void empilhar(Objeto* o, Pilha* P){
     printf("empilhando %c\n", o->valor);
     o->proximo_objeto = P->topo;
     P->topo = o;
-    P->quantidade++;
+    P->quantidade += 1;
 }
 
 Objeto* desempilhar(Pilha* P){
+    if(P->quantidade == 0){
+        return NULL;
+    }
     Objeto* o = P->topo;
-    printf("deempilhando %c\n", o->valor);
+    printf("desempilhando %c\n", o->valor);
     P->topo = o->proximo_objeto;
     P->quantidade--;
     return o;
